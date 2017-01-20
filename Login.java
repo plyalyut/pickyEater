67a4bd3aa903373474ed70357a400f01ee492bc8
plyalyut <pavlo_lyalyutskyy@brown.edu>
2017-01-19 21:43:56 -0500

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,17 +1,12 @@
 package pickyeater.pickyeaterandroid;
-
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.facebook.CallbackManager;
-import com.facebook.login.widget.LoginButton;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
@@ -25,26 +20,27 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.TwitterAuthProvider;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 
-import java.util.Arrays;
-
-
-public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener
-                                                         {
+public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener
+{
+    //cretes the google API client for authentication
     GoogleApiClient mGoogleApiClient;
     private FirebaseAuth mAuth; //intializing firebase
     private FirebaseAuth.AuthStateListener mAuthListener;
     private String TAG = "Sign In"; //tag for signing in
     private static final int RC_SIGN_IN = 9001; //what google likes to use for their sign-in's
+    private TwitterLoginButton twitterbutton;
 
     //create the sign-in screen
     @Override
@@ -52,87 +48,104 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                //if need more info use the get profile scope
+                .build();
 
-        //facebook login
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
-        //AppEventsLogger.activateApp(this); //logs the amount of times people log in
-        //CallbackManager callbackManager = CallbackManager.Factory.create();
-        //LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
 
-        //google login
-       // SignInButton googleButton = (SignInButton) findViewById(R.id.google_button);
-        //googleButton.setSize(SignInButton.SIZE_STANDARD);
-        //findViewById(R.id.google_button).setOnClickListener(this);
+        SignInButton signInButton = (SignInButton) findViewById(R.id.google_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        findViewById(R.id.google_button).setOnClickListener(this);
 
-        // Configure Google Sign In
-        //GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-         //       .requestIdToken(getString(R.string.default_web_client_id))
-          //      .requestEmail()
-          //      .build();
-
-        // Build a GoogleApiClient with access to the Google Sign-In API and the
-        // options specified by gso.
-
-        //mGoogleApiClient = new GoogleApiClient.Builder(this)
-          //      .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-            //    .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-        //        .build();
-
+        //twitter sign in
+        twitterbutton = (TwitterLoginButton) findViewById(R.id.twitter_button);
+        twitterbutton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(com.twitter.sdk.android.core.Result<TwitterSession> result) {
+                Log.d(TAG, "twitterLogin:success" + result);
+                handleTwitterSession(result.data);
+            }
+            @Override
+            public void failure(TwitterException exception) {
+                Log.w(TAG, "twitterLogin:failure", exception);
+            }
+        });
 
         //authentication listener
         mAuth = FirebaseAuth.getInstance();
-        if (mAuth.getCurrentUser() != null)
-        {
-            Intent intent = new Intent(this, MainMenu.class);
-            startActivity(intent);
-            Log.i("photo", mAuth.getCurrentUser().getPhotoUrl().toString());
-        }
-        else {
-            startActivityForResult(AuthUI.getInstance()
-        .createSignInIntentBuilder()
-                .setProviders((Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
-                        new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
-                        new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build(),
-                        new AuthUI.IdpConfig.Builder(AuthUI.TWITTER_PROVIDER).build()))
-                )
-                .setIsSmartLockEnabled(true)
-                .build(), RC_SIGN_IN
-        );}
-        //mAuthListener = new FirebaseAuth.AuthStateListener() {
-        //    @Override
-        //    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-        //        FirebaseUser user = firebaseAuth.getCurrentUser();
-         //       if (user != null) {
-         //           // User is signed in
-         //           Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-         //       } else {
-         //           // User is signed out
-           //         Log.d(TAG, "onAuthStateChanged:signed_out");
-         //     }
-          //  }};
+        mAuth.signOut();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            //checks to see if the user is already signed in
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    //todo send multiple pieces of data (name, image) to the new activity
+                    Intent intent = new Intent(getApplication(), MainMenu.class);
+                    startActivity(intent);
+                    //gets the photo
+                    Log.i("photo", mAuth.getCurrentUser().getPhotoUrl().toString());
+                    //user is signed in
+                    Log.d(TAG, "User Signed in:" + user.getUid());
+                } else {
+                    // User is signed out
 
-        //gets the google sign in button
+                    Log.d(TAG, "No user is signed in");
+                }
+            }
+        };}
 
+    //handle twitters request to login
+    private void handleTwitterSession(TwitterSession session) {
+        Log.d(TAG, "handleTwitterSession:" + session);
+
+        AuthCredential credential = TwitterAuthProvider.getCredential(
+                session.getAuthToken().token,
+                session.getAuthToken().secret);
+
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
-    /*
+    //click control scheme
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
         switch (v.getId()) {
             case R.id.google_button:
                 signIn();
                 break;
 
         }
-    }*/
+    }
 
-    /*
+    //sign in button for google
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
-    }*/
+    }
 
-    /*
+    //google activity result
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -149,67 +162,12 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                 // ...
             }
         }
-    }*/
-
-    /*
-    //methods to set up authentication
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-    */
-    /*
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
-    }*/
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-            // Successfully signed in
-            if (resultCode == ResultCodes.OK) {
-                Intent intent = new Intent(this, MainMenu.class);
-                startActivity(intent);
-                finish();
-                return;
-            } else {
-                // Sign in failed
-                if (response == null) {
-                    // User pressed back button
-                    //showSnackbar(R.string.sign_in_cancelled);
-                    return;
-                }
-
-                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
-                    //showSnackbar(R.string.no_internet_connection);
-                    return;
-                }
-
-                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
-                    //showSnackbar(R.string.unknown_error);
-                    return;
-                }
-            }
-
-            //showSnackbar(R.string.unknown_sign_in_response);
-        }
     }
 
-
-    //if connection fails to connect to google
-    @Override
-    public void onConnectionFailed(ConnectionResult result){
-        Log.d("GOOGLE", "Connection Failed");
-    }
-
-    /*
+    //firebase auth with google
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
         Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -227,6 +185,28 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                         }
                     }
                 });
-    }*/
+    }
 
+    //starts the state listener
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    //stops the state listener
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    //if connection fails to connect to google
+    @Override
+    public void onConnectionFailed(ConnectionResult result){
+        Log.d("GOOGLE", "Connection Failed");
+
+    }
 }
